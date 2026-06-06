aa1abb9ffbd5fc22ba730825dd4f2a57ada5600c
Yurii <hulivatiy.yura@gmail.com>
2026-06-06 22:15:03 +0300

    Created a skeleton for tracking apps in Windows

---
+++ winTracker/main.py
@@ -0,0 +1,34 @@
+import ctypes
+import win32con
+import pythoncom
+
+user32 = ctypes.windll.user32
+
+def callback(hook, event, hwnd, idObject, idChild, thread, time):
+    if hwnd: # the window ID
+        length = user32.GetWindowTextLengthW(hwnd) # needed for C-language as C requires a fixed size memory buffer
+        buffer = ctypes.create_unicode_buffer(length + 1)
+        user32.GetWindowTextW(hwnd, buffer, length + 1)
+        if buffer.value:
+            print("Event:", buffer.value)
+
+WinEventProc = ctypes.WINFUNCTYPE(
+    None, ctypes.c_void_p, ctypes.c_uint,
+    ctypes.c_void_p, ctypes.c_long, ctypes.c_long,
+    ctypes.c_uint, ctypes.c_uint
+)
+
+hook_cb = WinEventProc(callback)
+
+user32.SetWinEventHook(
+    win32con.EVENT_SYSTEM_FOREGROUND, # Starting range of events to track
+    win32con.EVENT_SYSTEM_FOREGROUND, # Ending range of events to track
+    0,
+    hook_cb,
+    0,
+    0,
+    win32con.WINEVENT_OUTOFCONTEXT
+)
+
+while True:
+    pythoncom.PumpWaitingMessages()
