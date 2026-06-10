fa4de76ce7362b42975aaba16a3c019a98140276
Yurii <hulivatiy.yura@gmail.com>
2026-06-10 23:40:26 +0300

    Reworked the processing pipeline. Added batch synchronization.

--- processing_layer/main.py
+++ processing_layer/main.py
@@ -5,83 +5,122 @@ from configs import TIMESTAMP_FORMAT, TIMESTAMP_MS_PRECISION
 
 """
 ToDo: split to Base+child classes
-tf
 """
+
 @dataclass
-class ActivityEvent:
-    process: str
+class Event:
     started_at: int
     ended_at: int
     duration_ms: int
-    id: int = None
+
+@dataclass
+class OperatingSystemEvent(Event):
+    process: str
+
+    def __repr__(self):
+        return f"OS Activity: {self.process=}, {self.started_at=}"
+
+@dataclass
+class BrowserEvent(Event):
+    os_event_id: int = None
     website: str | None = None
     website_title: str | None = None
+
+    def __repr__(self):
+        return f"Browser Activity: {self.website=}, {self.os_event_id=}"
 
 class EventProcessor:
     def __init__(self, repository):
         self.repository = repository
-        self.current: ActivityEvent = None
+        self.os_activity_last_row_id = self.repository.get_max_id("os_activity")
+        self.os_activities = []
+        self.browser_activities = []
+        self.batch_size = 5
 
     def handle_browser_event(self, payload):
-        if self.current is None:
-            return  # the object has to exist since browser is opened first
-
-        # Add the required fields for the website
-        self.current.website = payload.get("website")
-        self.current.website_title = payload.get("title")
-        started_at__formatted = datetime.datetime.fromtimestamp(
+        started_at = datetime.datetime.fromtimestamp(
             payload.get('started_at') / 1000
         ).strftime(TIMESTAMP_FORMAT)[:TIMESTAMP_MS_PRECISION] # Converts Unix-style timetamp to human-readable format 
-        self.current.started_at = started_at__formatted 
 
-        self.repository.insert_browser_activity(self.current)
-        logger.info(f"Browser Event: {payload.get('title')} - {started_at__formatted}")
+        event = BrowserEvent(
+            os_event_id=self.os_activity_last_row_id,
+            started_at=started_at,
+            ended_at=payload.get("ended_at"),
+            duration_ms=None,
+            website = payload.get("website"),
+            website_title = payload.get("title")
+        )
+
+        self.browser_activities.append(event)
+
+        
+        if len(self.browser_activities) > self.batch_size:  # fix data sync issue
+            self.repository.insert_browser_activities(self.browser_activities)
+            self.browser_activities = []
+        logger.info(f"Browser Event: {event.website} - {event.os_event_id}")
 
     def handle_os_event(self, payload):
-
         ended_at = datetime.datetime.now().strftime(TIMESTAMP_FORMAT)[:TIMESTAMP_MS_PRECISION]
-
-        self.current = ActivityEvent(
+        event = OperatingSystemEvent(
             process=payload.get("process"),
             started_at=payload.get("started_at"),
             ended_at=ended_at,
             duration_ms=None,
-
         )
 
-        last_row_id = self.repository.insert_os_activity(self.current)
-        self.current.id = last_row_id
-        logger.info(f"OS Event: {payload.get('process')} - {payload.get('started_at')}")
+        self.os_activities.append(event)
+        self.os_activity_last_row_id += 1
+        if len(self.os_activities) > self.batch_size:
+            self.repository.insert_os_activities(self.os_activities)
+            self.os_activities = []
+        logger.info(f"OS Event: {event.process} - {self.os_activity_last_row_id}")
     
 class ActivityRepository:
     def __init__(self, db):
         self.db = db
 
-    def insert_os_activity(self, activity):
-        self.db.execute("""
+    def insert_os_activities(self, activities):
+        cursor = self.db.executemany("""
             INSERT INTO os_activity
             (window, started_at)
             VALUES (?, ?)
         """, (
-            activity.process,
-            activity.started_at
+            [
+                (
+                    activity.process,
+                    activity.started_at
+                )
+                for activity in activities
+            ]
         ))
         self.db.commit()
-        return self.db.lastrowid
+        return cursor.lastrowid
 
-    def insert_browser_activity(self, activity): # not working properly
-        self
-        self.db.execute("""
+    def insert_browser_activities(self, activities): # not working properly
+        self.db.executemany("""
             INSERT INTO browser_activity
             (website, started_at, ended_at, duration_ms, activity_id)
             VALUES (?, ?, ?, ?, ?)
         """, (
-            activity.website,
-            activity.started_at,
-            activity.ended_at,
-            activity.duration_ms,
-            activity.id # id of parent activity 
+            [
+                (
+                    activity.website,
+                    activity.started_at,
+                    activity.ended_at,
+                    activity.duration_ms,
+                    activity.os_event_id
+                )
+                for activity in activities
+            ] 
             
         ))
 
         self.db.commit()
+    
+    def get_max_id(self, table_name):
+        allowed_tables = ["os_activity"]
+        if table_name not in allowed_tables:
+            raise ValueError("Unknown table name.")
+
+        cursor = self.db.execute(f"SELECT MAX(seq) FROM sqlite_sequence WHERE name=\'{table_name}\'")
+        return cursor.fetchone()[0] or 0
